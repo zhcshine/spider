cf93a6254cb75b8775d4d08f64bca308f4ebd9d8
zhcshine <zhcshine@gmail.com>
2016-11-23 08:52:00 +0800

    修正bug,多进程不会同时处理一个url地址

--- UrlManager/UrlManager.py
+++ UrlManager/UrlManager.py
@@ -28,6 +28,16 @@ class UrlManager:
         else:
             print u'剩余爬取数量: ' + str(results)
             record = db_connect().fetch_one()
+            # 锁住此url，防止多进程运行时重复读取
+            sql_data = {
+                'is_crawed': 2,
+            }
+            condition = 'id = {}'.format(record[0])
+            try:
+                db_connect().update(table, sql_data, condition)
+                db_connect().commit()
+            except Exception, e:
+                pass
             result = {
                 'id': record[0],
                 'pid': record[1],
